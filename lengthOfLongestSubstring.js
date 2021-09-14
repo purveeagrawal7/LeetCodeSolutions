cf7a154c1002b03cf8ef9f2df212e5658ccb028c
Purvee Agrawal <purvee.agrawal@gmail.com>
2021-09-14 08:02:17 -0700

    Update lengthOfLongestSubstring.js

--- lengthOfLongestSubstring.js
+++ lengthOfLongestSubstring.js
@@ -1,1 +1,19 @@
+var lengthOfLongestSubstring = function(s) {
+    var max = 0;
+    var maxLen = 0;
+    var longSubStr = "";
+    for(let i =0; i<s.length;i++){
+        if(longSubStr.indexOf(s[i]) !== -1){
+            longSubStr = longSubStr.substr(longSubStr.indexOf(s[i])+1, longSubStr.length);
+            max = longSubStr.length;
+        }
+        longSubStr += s[i]
+        max += 1;
+        if(max > maxLen){
+            maxLen = max; 
+        }
+    }
+    return maxLen;
+};
 
+lengthOfLongestSubstring("dvdf");
